3fadcfb99618dc5dafd4cb7e51ddc65ed182211c
volodymyr_makarenko <hhvolodymyrhh@gmail.com>
2024-06-24 10:54:27 +0300

    edit operand ||

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,9 @@
 "use strict";
 function checkForSpam(message){
-    if (message.toLowerCase().includes("spam")) {
+    if (message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale")) {
         return true;
     }
-     else if(message.toLowerCase().includes("sale")) {
-        return true;
-    }
+    
     else {
         return false;
     }
